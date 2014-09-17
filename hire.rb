a667f9146e1d31bb9f409ef19a0d421c8d024acc
Sam Soffes <sam@soff.es>
2014-09-17 09:58:49 +0200

    Closed

--- hire.rb
+++ hire.rb
@@ -4,23 +4,6 @@ $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.passwo
 
 class Hire < Sinatra::Base
   get '/' do
-    @weeks = []
-    start_date = Chronic.parse('Monday', context: :past)
-    end_date = Chronic.parse('next Sunday', now: start_date)
-    today = Time.now
-
-    12.times do |i|
-      start_string = start_date.strftime('%b %e').gsub('  ', ' ')
-      @weeks << {
-        booked: (end_date < today or $redis.get(start_string)),
-        start_date: start_string,
-        end_date: end_date.strftime('%b %e').gsub('  ', ' ')
-      }
-
-      start_date = Chronic.parse('next Monday', now: end_date)
-      end_date = Chronic.parse('next Sunday', now: start_date)
-    end
-
-    erb :index, locals: { weeks: @weeks }
+    erb :index
   end
 end
