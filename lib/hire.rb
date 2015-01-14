5a82545215840cc5c8e668e12a60ea34d6140d7b
Sam Soffes <sam@soff.es>
2015-01-14 12:25:54 -0800

    Switch to REDIS_URL

--- lib/hire.rb
+++ lib/hire.rb
@@ -5,7 +5,7 @@ module Hire
 
   def redis
     $redis ||= begin
-      uri = URI.parse((ENV['REDISTOGO_URL'] || 'redis://localhost:6379'))
+      uri = URI.parse((ENV['REDIS_URL'] || 'redis://localhost:6379'))
       Redis.new(host: uri.host, port: uri.port, password: uri.password)
     end
   end
